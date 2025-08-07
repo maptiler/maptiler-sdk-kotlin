b1849492a33c24a9a470654b86fd05e2d288b43a
sasaprodribaba <sasa.prodribaba@maptiler.com>
2025-08-07 09:08:26 +0200

    RD-1079: Add MTLayer interface

---
+++ MapTilerSDK/src/main/java/com/maptiler/maptilersdk/map/style/layer/MTLayer.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025, MapTiler
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD 3-Clause
+ */
+
+package com.maptiler.maptilersdk.map.style.layer
+
+/**
+ * Protocol requirements for all types of Layers.
+ */
+interface MTLayer {
+    /**
+     * Unique id of the layer.
+     */
+    var identifier: String
+
+    /**
+     * Type of the layer.
+     */
+    var type: MTLayerType
+
+    /**
+     * Identifier of the source.
+     */
+    var sourceIdentifier: String
+
+    /**
+     * Max zoom of the layer.
+     */
+    var maxZoom: Double?
+
+    /**
+     * Min zoom of the layer.
+     */
+    var minZoom: Double?
+
+    /**
+     * Identifier of the source (main) layer to use.
+     */
+    var sourceLayer: String?
+}
