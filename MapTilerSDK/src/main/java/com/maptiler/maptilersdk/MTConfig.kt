d12f54cdb961e744c762067cdf42da4547babe2f
klokantechsysadmin <10697954+klokantechsysadmin@users.noreply.github.com>
2025-11-04 09:37:48 +0000

    [create-pull-request] automated change

--- MapTilerSDK/src/main/java/com/maptiler/maptilersdk/MTConfig.kt
+++ MapTilerSDK/src/main/java/com/maptiler/maptilersdk/MTConfig.kt
@@ -39,7 +39,7 @@ object MTConfig {
     /**
      * Boolean indicating whether session logic is enabled.
      *
-     * This allows MapTiler to enable "session based billing".
+     * This allows MapTiler to enable session-based billing.
      * Defaults to true.
      * For more information about sessions, see [Map Sessions](https://docs.maptiler.com/guides/maps-apis/maps-platform/what-is-map-session-in-maptiler-cloud/).
      */
@@ -51,10 +51,10 @@ object MTConfig {
      * The telemetry is very valuable to the team at MapTiler because it shares information about
      * where to add extra effort. It also helps spot some incompatibility issues that may
      * arise between the SDK and a specific version of a module. It consists of sending the SDK version,
-     * API Key, MapTiler session ID, if tile caching is enabled, if language specified at initialization,
-     * if terrain is activated at initialization, if globe projection is activated at initialization.
+     * API Key, MapTiler session ID, whether tile caching is enabled, whether a language is specified at initialization,
+     * whether terrain is activated at initialization, and whether globe projection is activated at initialization.
      * Defaults to true.
-     * @see 'https://docs.maptiler.com/guides/maps-apis/maps-platform/what-is-map-session-in-maptiler-cloud/'
+     * @see <https://docs.maptiler.com/guides/maps-apis/maps-platform/what-is-map-session-in-maptiler-cloud/>
      */
     var isTelemetryEnabled: Boolean = true
 }
