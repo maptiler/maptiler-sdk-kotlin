2828f97f780c54e24fcb8d10a649ef4a36b0c462
klokantechsysadmin <10697954+klokantechsysadmin@users.noreply.github.com>
2026-05-29 16:23:14 +0000

    [create-pull-request] automated change

---
+++ MapTilerSDK/src/main/java/com/maptiler/maptilersdk/commands/misc/LngLatToArray.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025, MapTiler
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD 3-Clause
+ */
+
+package com.maptiler.maptilersdk.commands.misc
+
+import com.maptiler.maptilersdk.bridge.JSString
+import com.maptiler.maptilersdk.bridge.MTBridge
+import com.maptiler.maptilersdk.bridge.MTCommand
+import com.maptiler.maptilersdk.map.LngLat
+
+internal data class LngLatToArray(
+    val lngLat: LngLat,
+) : MTCommand {
+    override val isPrimitiveReturnType: Boolean = true
+
+    override fun toJS(): JSString = "new ${MTBridge.SDK_OBJECT}.LngLat(${lngLat.lng}, ${lngLat.lat}).toArray();"
+}
