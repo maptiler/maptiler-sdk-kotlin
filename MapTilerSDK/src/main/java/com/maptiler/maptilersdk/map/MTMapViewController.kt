ead7e6f110778cfc89257fd61b47fc2607812864
klokantechsysadmin <10697954+klokantechsysadmin@users.noreply.github.com>
2026-08-25 10:53:51 +0000

    [create-pull-request] automated change

--- MapTilerSDK/src/main/java/com/maptiler/maptilersdk/map/MTMapViewController.kt
+++ MapTilerSDK/src/main/java/com/maptiler/maptilersdk/map/MTMapViewController.kt
@@ -61,6 +61,7 @@ import com.maptiler.maptilersdk.map.style.MTStyle
 import com.maptiler.maptilersdk.map.types.MTBounds
 import com.maptiler.maptilersdk.map.types.MTData
 import com.maptiler.maptilersdk.map.types.MTLanguage
+import com.maptiler.maptilersdk.map.types.MTMapCorner
 import com.maptiler.maptilersdk.map.types.MTPoint
 import com.maptiler.maptilersdk.map.types.MTProjectionType
 import com.maptiler.maptilersdk.map.workers.navigable.MTNavigable
@@ -426,6 +427,32 @@ class MTMapViewController(
     fun showTileBoundaries(show: Boolean) {
         coroutineScope?.launch {
             bridge?.execute(com.maptiler.maptilersdk.commands.misc.ShowTileBoundaries(show))
+        }
+    }
+
+    /**
+     * Adds the MapTiler navigation control to the map.
+     *
+     * @param position The position of the control.
+     * @param showCompass Show compass button.
+     * @param showZoom Show zoom buttons.
+     * @param visualizePitch Visualize pitch on the compass.
+     */
+    fun addNavigationControl(
+        position: MTMapCorner = MTMapCorner.TOP_RIGHT,
+        showCompass: Boolean = true,
+        showZoom: Boolean = true,
+        visualizePitch: Boolean = false,
+    ) {
+        coroutineScope?.launch {
+            bridge?.execute(
+                com.maptiler.maptilersdk.commands.misc.AddNavigationControl(
+                    showCompass,
+                    showZoom,
+                    visualizePitch,
+                    position,
+                ),
+            )
         }
     }
 
